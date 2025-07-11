a3f59f34a800a58cf9c3170acf8f1a2dac9f2719
Gustavo Rocha <53187330+AzitromicinA@users.noreply.github.com>
2025-07-11 12:53:30 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function Header() {
   return (
     <header className="flex justify-between items-center py-6 px-8 bg-white shadow">
-      <h1 className="text-xl font-bold">Template 03</h1>
       <nav className="space-x-4">
         <a href="#" className="text-gray-700 hover:underline">Projetos</a>
         <a href="#" className="text-gray-700 hover:underline">Blog</a>
