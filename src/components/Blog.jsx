69e89e894286f51d1e12b8d0a32ec316e2bd1b51
Gustavo Rocha <53187330+AzitromicinA@users.noreply.github.com>
2025-07-11 12:48:33 -0300

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -5,13 +5,13 @@ const posts = [
     title: "Making a design system from scratch",
     date: "12 Feb 2020",
     tags: ["Design", "Pattern"],
-    summary: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+    summary: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
   },
   {
     title: "Creating pixel perfect icons in Figma",
     date: "12 Feb 2020",
     tags: ["Figma", "Icon Design"],
-    summary: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+    summary: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
   }
 ];
 
